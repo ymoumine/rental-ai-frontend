a2c17206dbc89e755455078073bae3b015318ee9
ymoumin <ymoum044@uottawa.ca>
2025-04-02 10:42:24 -0400

    using fetch client for server-to-server avoiding cron middleware

--- app/api/cron/route.js
+++ app/api/cron/route.js
@@ -1,5 +1,4 @@
 import { NextResponse } from 'next/server';
-import axios from 'axios';
 
 const apiURL = process.env.NEXT_PUBLIC_API_URL;
 const mlURL = process.env.NEXT_PUBLIC_ML_API_URL;
@@ -9,8 +8,14 @@ const mlURL = process.env.NEXT_PUBLIC_ML_API_URL;
 // TO DO: then add secret key to cron job
 export async function GET() {
     try {
-        const apiResponse = await axios.get(`${apiURL}/api/home`);
-        const mlResponse = await axios.get(`${mlURL}/api/get_test_data`);
+        const apiResponse = await fetch(`${apiURL}/api/home`);
+        if (!apiResponse.ok) {
+            return NextResponse.json({ ok: false });
+        }
+        const mlResponse = await fetch(`${mlURL}/api/get_test_data`);
+        if (!mlResponse.ok) {
+            return NextResponse.json({ ok: false });
+        }
         return NextResponse.json({ ok: true });
     } catch (error) {
         return NextResponse.json({ ok: false });
